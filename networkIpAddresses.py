8931423abadc985138c8fedbf3bf8a3b975571b5
kennethbrandonp <70538624+kennethbrandonp@users.noreply.github.com>
2022-12-07 20:41:12 -0800

    augments to network listing

--- networkIpAddresses.py
+++ networkIpAddresses.py
@@ -8,13 +8,25 @@ class Network(object):
 
         #However 192.168.0.1 and 192.168.1.1 are default gateways
 
-        ip = '192.168.1.1' #I use 1.1 instead of 0.1 because no lists are displayed using 0.1
-        print(ip)
+        ip = '192.168.1.1' 
         self.ip = ip
+        self.host_list_size = None
 
-    def networkscanner(self):
+    def networkCheck(self):
+        if(self.host_list_size == 0):
+            if (self.ip == '192.168.1.1'):
+                self.ip = '192.168.1.0'
+
+            elif(self.ip == '192.168.1.0'):
+                self.ip = '192.168.1.1'
+
+    def networkCounter(self, host):
+        self.host_list_size = len(host) #Counts the amount of connected devices in a network.
+
+    def networkScanner(self):
         if len(self.ip) == 0:
             print("No IP address given")  #If there's no ip given
+
         else:
             network = self.ip + '/24'
             print("Scanning IPs") #this takes awhile.
@@ -25,9 +37,17 @@ class Network(object):
             #Stores a list of every ip address on the network into host_list
             host_list = [(x, nm[x] ['status'] ['state']) for x in nm.all_hosts()]
 
-            for host in host_list:
-                print("Host\t{}".format(host))
+            self.networkCounter(host_list)
+
+            self.networkCheck()
+
+            for host, up in host_list:
+                if(host == self.ip):
+                    print("\n List of connected devices:  ")
+                    print("_______________________________")
+                else:
+                    print(host)
 
 if __name__ == "__main__":
     network = Network()
-    network.networkscanner()
+    network.networkScanner()
